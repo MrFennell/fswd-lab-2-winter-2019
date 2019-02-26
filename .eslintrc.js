69a34b9c23e3fd7007067b8b814e7e91ee697648
Ryan <rfennell001@gmail.com>
2019-02-25 20:41:30 -0500

    yml fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  parserOptions: { ecmaVersion: 2017 },
-  env: {
-    node: true,
-    es6: true
-  },
-  extends: ['eslint:recommended']
-};
+    rules: {
+        'no-unused-vars': 0
+    }
+}; 
